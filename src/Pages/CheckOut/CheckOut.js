e93b5145faa39f6469153ea555dd0bc856fb1eb6
mdriazuddin417 <mdriazuddin417@gmail.com>
2022-04-17 23:33:26 +0600

    checkout section complete

--- src/Pages/CheckOut/CheckOut.js
+++ src/Pages/CheckOut/CheckOut.js
@@ -1,6 +1,8 @@
 import React from "react";
+import useServices from "../../hooks/useServices";
 
-const CheckOut = () => {
+const CheckOut = (props) => {
+  const [services, setServices] = useServices();
   return (
     <div className="h-[80vh] grid lg:grid-cols-3 gap-4 px-12 mx-auto max-w-7xl bg-gray-300">
       <div className="lg:col-span-2 ...">
@@ -28,9 +30,59 @@ const CheckOut = () => {
         </div>
         <div className="w-100 bg-white rounded-lg py-3 px-4 mt-3">
           <h2 className="text-xl font-bold">Order Overview</h2>
+          {services.slice(0, 1).map((service) => (
+            <div>
+              <div className="flex gap-4">
+                <img
+                  src={service.picture}
+                  alt=""
+                  width={300}
+                  className="rounded-lg"
+                />
+                <div>
+                  <p className="text-xl font-bold">{service.name}</p>
+                  <p className="text-xl text-gray-700">{service.text}</p>
+                  <p className="text-2xl font-bold ">{service.price}</p>
+                </div>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
-      <div className="..."></div>
+      <div className="... h-100">
+        <div className="w-100 bg-white rounded-lg py-3 px-4 mt-3 ">
+          <h2 className="text-xl font-bold">Order Summary</h2>
+          <p className="text-blue-500">
+            <select name="" id="" className="mb-5">
+              <option value="">Select Coupon</option>
+              <option value="">021154</option>
+              <option value="">3241645</option>
+              <option value="">324124</option>
+            </select>
+            <br />
+            <select name="" id="">
+              <option value="">All Express Coupon</option>
+              <option value="">021154</option>
+              <option value="">3241645</option>
+              <option value="">324124</option>
+            </select>
+          </p>
+          <div className="mb-5">
+            <input
+              type="text"
+              placeholder="Promo code"
+              className="bg-gray-200 p-2 rounded-md"
+            />
+            <button className="bg-red-400 text-black px-1 py-2 rounded-lg">
+              Apply
+            </button>
+          </div>
+          <p className="text-2xl font-bold mb-2">Total:$ 31</p>
+          <button className="bg-orange-400 text-white rounded w-100 py-2">
+            Place Order
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
--- src/Pages/HomePages/Service/Service.js
+++ src/Pages/HomePages/Service/Service.js
@@ -1,18 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
 import { Card } from "react-bootstrap";
 import { CgShoppingCart } from "react-icons/cg";
 import { Link } from "react-router-dom";
 
 const Service = ({ service }) => {
-  const [cart, setCart] = useState([]);
-
   const { price, name, id, picture, text } = service;
 
-  const handleService = (service) => {
-    setCart(service);
-    console.log(service);
-  };
-  console.log(cart);
   return (
     <div>
       <Card className="h-100 rounded-2xl border-0 shadow">
@@ -22,12 +15,7 @@ const Service = ({ service }) => {
           <p className="text-2xl text-blue-600 font-bold">{price}</p>
           <Card.Text className="text-gray-400">{text}</Card.Text>
           <Link to={"/checkout"} className="decoration-transparent">
-            <button
-              className="bg-green-500 text-white  py-2 rounded-2xl w-100 flex justify-center items-center text-xl hover:bg-green-800 hover:text-black hover:transition "
-              onClick={() => {
-                handleService(service);
-              }}
-            >
+            <button className="bg-green-500 text-white  py-2 rounded-2xl w-100 flex justify-center items-center text-xl hover:bg-green-800 hover:text-black hover:transition ">
               Order Now <CgShoppingCart className="text-2xl ms-2 " />
             </button>
           </Link>
